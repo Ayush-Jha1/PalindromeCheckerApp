c1a5470c3b110775661f1aaa237c12921dac5cf1
Ayush-Jha1 <ayushjhamail95@gmail.com>
2026-02-23 16:22:55 +0530

    UC2: Hardcoded palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -9,13 +9,19 @@ public class PalindromeCheckerApp {
     // Main Method - Entry point of the Java Application
     public static void main(String[] args) {
 
-        System.out.println("==============================================");
-        System.out.println("        WELCOME TO PALINDROME CHECKER APP     ");
-        System.out.println("==============================================");
-        System.out.println("Application Version : 1.0");
-        System.out.println("Description : This application checks whether");
-        System.out.println("a given string is a palindrome.");
-        System.out.println("==============================================");
-        System.out.println("Application started successfully...");
+        String word = "madam";   // Hardcoded string
+        String reversed = "";
+
+        // Reverse the string
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check palindrome condition
+        if (word.equals(reversed)) {
+            System.out.println("The word '" + word + "' is a Palindrome.");
+        } else {
+            System.out.println("The word '" + word + "' is NOT a Palindrome.");
+        }
     }
 }
